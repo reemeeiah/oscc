58cbe06ea7b4c7daa23587ad557cf4ea0f4ed4ab
alseraj <alsrajmax@gmail.com>
2018-03-16 18:18:46 +0300

    go

--- app/src/main/java/com/oscc/oscc/UserFragment.java
+++ app/src/main/java/com/oscc/oscc/UserFragment.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -57,6 +56,7 @@ public class UserFragment extends Fragment {
 
 
         return v;
+
     }
     public void setStories()
     {
